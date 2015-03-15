afaa59428c7129681d25f53afd92ba9304a3a6ef
Mauro Silva <maurosilva@ifma.edu.br>
2015-03-15 14:53:37 -0300

    Mandando tudo.

--- TaBaratoMesmo/src/com/atompxk/tabaratomesmo/JanelaPrincipal.java
+++ TaBaratoMesmo/src/com/atompxk/tabaratomesmo/JanelaPrincipal.java
@@ -12,6 +12,7 @@ public class JanelaPrincipal extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_janela_principal);
+        //Comentario de uma linha
     }
 
 
