6f3e1a9c5ea634e2be78b5f308dad6ac6a72f47a
Mauro Silva <maurosilva@ifma.edu.br>
2015-03-15 11:21:57 -0300

    Mudança apenas para testar.

--- TaBarato/src/com/atom/tabarato/JanelaPrincipal.java
+++ TaBarato/src/com/atom/tabarato/JanelaPrincipal.java
@@ -12,6 +12,7 @@ public class JanelaPrincipal extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_janela_principal);
+        //comentario apenas para testar.
     }
 
 
