a24309701c28da8b5df476e83c36fffe7592bea5
Mauro Silva <maurosilva@ifma.edu.br>
2015-03-15 21:55:19 -0300

    Mudança do código apenas para testar.

--- Caderneta/src/com/atompxk/caderneta/TelaInicial.java
+++ Caderneta/src/com/atompxk/caderneta/TelaInicial.java
@@ -12,6 +12,7 @@ public class TelaInicial extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_inicial);
+        //Comentário pra testar.
     }
 
 
