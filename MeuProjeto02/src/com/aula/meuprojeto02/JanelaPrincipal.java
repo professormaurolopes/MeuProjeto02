ad9a00cfcfadf3c071d9c481d73c8b2f6b03b7dd
Mauro Silva <maurosilva@ifma.edu.br>
2015-03-19 20:39:49 -0300

    Projeto Inicial

---
+++ MeuProjeto02/src/com/aula/meuprojeto02/JanelaPrincipal.java
@@ -0,0 +1,65 @@
+package com.aula.meuprojeto02;
+
+import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class JanelaPrincipal extends ActionBarActivity implements OnClickListener {
+
+    private EditText tnome;
+    private Button bMsg;
+    
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_janela_principal);
+        tnome = (EditText) findViewById(R.id.txtNome);
+        bMsg = (Button) findViewById(R.id.btnMensagem);
+        bMsg.setOnClickListener(this);
+        
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.janela_principal, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		String nome;
+		nome = tnome.getText().toString();
+		//Toast.makeText(this, nome, Toast.LENGTH_SHORT).show();
+		Intent it = new Intent(this, Janela02.class);
+		Bundle parametros = new Bundle();
+		parametros.putString("nome", nome);
+		it.putExtras(parametros);
+		startActivity(it);
+		
+	}
+}
